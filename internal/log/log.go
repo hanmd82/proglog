843c1c41c58464736a8bd44d3dc3dbcbbccccff8
Mingding, Han <hanmd82@gmail.com>
2020-04-20 23:54:10 +0800

    feat: Add code and tests for log struct
    - this completes Chapter 3

---
+++ internal/log/log.go
@@ -0,0 +1,195 @@
+package log
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+
+	api "github.com/hanmd82/proglog/api/v1"
+)
+
+type Log struct {
+	mu sync.RWMutex
+
+	Dir    string
+	Config Config
+
+	activeSegment *segment
+	segments      []*segment
+}
+
+// Set up a new Log for the segments that already exist on disk.
+// If the log is new and has no existing segments, bootstrap the initial segment.
+func NewLog(dir string, c Config) (*Log, error) {
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+
+	l := &Log{
+		Dir:    dir,
+		Config: c,
+	}
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var baseOffsets []uint64
+	for _, file := range files {
+		offStr := strings.TrimSuffix(
+			file.Name(),
+			path.Ext(file.Name()),
+		)
+		off, _ := strconv.ParseUint(offStr, 10, 0)
+		baseOffsets = append(baseOffsets, off)
+	}
+
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+
+	for i := 0; i < len(baseOffsets); i++ {
+		if err = l.newSegment(baseOffsets[i]); err != nil {
+			return nil, err
+		}
+		i++
+	}
+
+	if l.segments == nil {
+		if err = l.newSegment(c.Segment.InitialOffset); err != nil {
+			return nil, err
+		}
+	}
+	return l, nil
+}
+
+// Append(*api.Record) appends a record to the log
+func (l *Log) Append(record *api.Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	off, err := l.activeSegment.Append(record)
+	if err != nil {
+		return 0, err
+	}
+
+	if l.activeSegment.IsMaxed() {
+		err = l.newSegment(off + 1)
+	}
+	return off, err
+}
+
+// Read(uint64) reads the record stored at the given offset.
+func (l *Log) Read(off uint64) (*api.Record, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	var s *segment
+	for _, segment := range l.segments {
+		if segment.baseOffset <= off {
+			s = segment
+			break
+		}
+	}
+
+	if s == nil || s.nextOffset <= off {
+		return nil, fmt.Errorf("offset out of range: %d", off)
+	}
+
+	return s.Read(off)
+}
+
+// newSegment(uint64) creates a new segment, appends to the log’s slice of segments, and
+// makes the new segment the active segment so that subsequent append calls write to it.
+func (l *Log) newSegment(off uint64) error {
+	s, err := newSegment(l.Dir, off, l.Config)
+	if err != nil {
+		return err
+	}
+
+	l.segments = append(l.segments, s)
+	l.activeSegment = s
+	return nil
+}
+
+// Close() iterates over the segments and closes them.
+func (l *Log) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	for _, segment := range l.segments {
+		if err := segment.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Remove() closes the log and then removes its data.
+func (l *Log) Remove() error {
+	if err := l.Close(); err != nil {
+		return err
+	}
+	return os.RemoveAll(l.Dir)
+}
+
+// Reset() removes the log and then creates a new log to replace it.
+func (l *Log) Reset() error {
+	if err := l.Remove(); err != nil {
+		return err
+	}
+
+	var err error
+	new, err := NewLog(l.Dir, l.Config)
+	if err != nil {
+		return err
+	}
+
+	*l = *new
+	return nil
+}
+
+func (l *Log) LowestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.segments[0].baseOffset, nil
+}
+
+func (l *Log) HighestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	off := l.segments[len(l.segments)-1].nextOffset
+	if off == 0 {
+		return 0, nil
+	}
+	return off - 1, nil
+}
+
+// Truncate(uint64) removes all segments whose highest offset is lower than lowest.
+func (l *Log) Truncate(lowest uint64) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	var segments []*segment
+	for _, s := range l.segments {
+		if s.nextOffset <= lowest+1 {
+			if err := s.Remove(); err != nil {
+				return err
+			}
+			continue
+		}
+		segments = append(segments, s)
+	}
+	l.segments = segments
+	return nil
+}
---
+++ internal/log/log_test.go
@@ -0,0 +1,80 @@
+package log_test
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	api "github.com/hanmd82/proglog/api/v1"
+	"github.com/hanmd82/proglog/internal/log"
+	"github.com/stretchr/testify/require"
+)
+
+// TestLog(*testing.T) defines a table of tests
+func TestLog(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T, log *log.Log,
+	){
+		"append and read a record":    testAppendRead,
+		"offset out of range error":   testOutOfRangeErr,
+		"init with existing segments": testInitExisting,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			dir, err := ioutil.TempDir("", "store-test")
+			require.NoError(t, err)
+			defer os.RemoveAll(dir)
+
+			c := log.Config{}
+			c.Segment.MaxStoreBytes = 32
+
+			log, err := log.NewLog(dir, c)
+			require.NoError(t, err)
+
+			fn(t, log)
+		})
+	}
+}
+
+// testAppendRead(*testing.T, *log.Log) tests successfully appending to and reading from the log.
+func testAppendRead(t *testing.T, log *log.Log) {
+	record := &api.Record{
+		Value: []byte("hello world"),
+	}
+
+	off, err := log.Append(record)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, record, read)
+}
+
+// testOutOfRangeErr(*testing.T, *log.Log) tests that the log returns an error when trying to read an offset
+// that’s outside of the range of offsets the log has stored.
+func testOutOfRangeErr(t *testing.T, log *log.Log) {
+	read, err := log.Read(1)
+	require.Nil(t, read)
+	require.Error(t, err)
+}
+
+// testInitExisting(*testing.T, *log.Log) tests that creating a log bootstraps
+// from the data stored by prior log instances.
+func testInitExisting(t *testing.T, o *log.Log) {
+	record := &api.Record{
+		Value: []byte("hello world"),
+	}
+
+	for i := 0; i < 3; i++ {
+		_, err := o.Append(record)
+		require.NoError(t, err)
+	}
+	require.NoError(t, o.Close())
+
+	n, err := log.NewLog(o.Dir, o.Config)
+	require.NoError(t, err)
+
+	off, err := n.Append(record)
+	require.NoError(t, err)
+	require.Equal(t, uint64(3), off)
+}
