5fef4e1936db45c0a08eb3edbd646ead10157748
Mingding, Han <hanmd82@gmail.com>
2020-04-26 00:00:07 +0800

    feat: Add tests and complete chapter4

---
+++ internal/server/server_test.go
@@ -0,0 +1,188 @@
+package server
+
+import (
+	"context"
+	"io/ioutil"
+	"net"
+	"testing"
+
+	api "github.com/hanmd82/proglog/api/v1"
+	"github.com/hanmd82/proglog/internal/log"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func TestServer(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T,
+		client api.LogClient,
+		config *Config,
+	){
+		"produce/consume a message to/from the log succeeds": testProduceConsume,
+		"produce/consume stream succeeds":                    testProduceConsumeStream,
+		"consume past log boundary fails":                    testConsumePastBoundary,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			client, config, teardown := testSetup(t, nil)
+			defer teardown()
+			fn(t, client, config)
+		})
+	}
+}
+
+// testSetup(*testing.T, func(*Config)) is a helper function to set up each test case.
+func testSetup(t *testing.T, fn func(*Config)) (
+	client api.LogClient,
+	config *Config,
+	teardown func(),
+) {
+	t.Helper()
+
+	l, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+	clientOptions := []grpc.DialOption{grpc.WithInsecure()}
+	cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
+	require.NoError(t, err)
+
+	dir, err := ioutil.TempDir("", "server-test")
+	require.NoError(t, err)
+
+	clog, err := log.NewLog(dir, log.Config{})
+	require.NoError(t, err)
+
+	config = &Config{
+		CommitLog: clog,
+	}
+
+	if fn != nil {
+		fn(config)
+	}
+	server, err := NewGRPCServer(config)
+	require.NoError(t, err)
+
+	go func() {
+		server.Serve(l)
+	}()
+
+	client = api.NewLogClient(cc)
+
+	return client, config, func() {
+		server.Stop()
+		cc.Close()
+		l.Close()
+	}
+}
+
+func testProduceConsume(
+	t *testing.T,
+	client api.LogClient,
+	config *Config,
+) {
+	ctx := context.Background()
+
+	want := &api.Record{
+		Value: []byte("hello world"),
+	}
+
+	produce, err := client.Produce(
+		context.Background(),
+		&api.ProduceRequest{
+			Record: want,
+		},
+	)
+	require.NoError(t, err)
+
+	consume, err := client.Consume(
+		ctx,
+		&api.ConsumeRequest{
+			Offset: produce.Offset,
+		},
+	)
+	require.NoError(t, err)
+	require.Equal(t, want, consume.Record)
+}
+
+// testConsumePastBoundary(*testing.T, api.LogClient, *Config) tests that the Log server
+// responds with an api.ErrOffsetOutOfRange() error when a client tries to consume beyond the log’s boundaries.
+func testConsumePastBoundary(
+	t *testing.T,
+	client api.LogClient,
+	config *Config,
+) {
+	ctx := context.Background()
+
+	produce, err := client.Produce(
+		ctx,
+		&api.ProduceRequest{
+			Record: &api.Record{
+				Value: []byte("hello world"),
+			},
+		},
+	)
+	require.NoError(t, err)
+
+	consume, err := client.Consume(
+		ctx,
+		&api.ConsumeRequest{
+			Offset: produce.Offset + 1,
+		},
+	)
+	if consume != nil {
+		t.Fatal("consume not nil")
+	}
+
+	got := grpc.Code(err)
+	want := grpc.Code(api.ErrOffsetOutOfRange{}.GRPCStatus().Err())
+	if got != want {
+		t.Fatalf("got err: %v, want: %v", got, want)
+	}
+}
+
+func testProduceConsumeStream(
+	t *testing.T,
+	client api.LogClient,
+	config *Config,
+) {
+	ctx := context.Background()
+
+	records := []*api.Record{
+		{Value: []byte("first message")},
+		{Value: []byte("second message")},
+	}
+	{
+		stream, err := client.ProduceStream(ctx)
+		require.NoError(t, err)
+
+		for offset, record := range records {
+			err = stream.Send(&api.ProduceRequest{
+				Record: record,
+			})
+			require.NoError(t, err)
+
+			res, err := stream.Recv()
+			require.NoError(t, err)
+
+			if res.Offset != uint64(offset) {
+				t.Fatalf(
+					"got offset: %d, want: %d",
+					res.Offset,
+					offset,
+				)
+			}
+		}
+	}
+	{
+		stream, err := client.ConsumeStream(
+			ctx,
+			&api.ConsumeRequest{Offset: 0},
+		)
+		require.NoError(t, err)
+
+		for _, record := range records {
+			res, err := stream.Recv()
+			require.NoError(t, err)
+			require.Equal(t, res.Record, record)
+		}
+	}
+}
